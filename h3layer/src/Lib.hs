0153596c37d2cf3df7c051c730e13f87e645c5de
Debasish Ghosh <ghosh.debasish@gmail.com>
2020-11-20 12:42:01 +0530

    No more orphan instances

--- h3layer/src/Lib.hs
+++ h3layer/src/Lib.hs
@@ -1,25 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Lib ( main ) where
 
-import qualified Lib.Repository.UserRepo as UserRepo
-import qualified Lib.Service.UserService as UserService
-
-import Lib.App (App, AppEnv, Env (..), WithError)
+import Lib.App (AppEnv, Env (..), WithError)
 import Lib.Config (Config (..), loadConfig)
 import Lib.Core.Email (Email (..))
 import Lib.Db (initialisePool, WithDb)
-import Lib.Repository (getUserByEmail)
 import Lib.Service (getUserName)
 import Lib.Effects.Log (mainLogAction, runAppLogIO_)
 
-
-instance UserRepo.UserRepo App where
-  getUserByEmail = Lib.Repository.getUserByEmail
-
-instance UserService.UserService App where
-  getUserName = Lib.Service.getUserName
 
 mkAppEnv :: Config -> IO AppEnv
 mkAppEnv Config{..} = do
